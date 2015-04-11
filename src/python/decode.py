2f08b2e96546cc9669ae14bfc4eb511ad3d07133
Laura Shea <laura_shea@me.com>
2015-04-11 17:04:55 -0400

    updated encode/decode

--- src/python/decode.py
+++ src/python/decode.py
@@ -1,23 +1,19 @@
-
+#!/usr/bin/env python
 import sys
 import subprocess
 
 # INPUT:
-# Takes in the name of a video to read (videoin) and a resolution
-# (res).
+# Takes in the name of a video to read (videoin) and a
+# resolution (res). 
 
 # OUTPUT:
-# Doesn't return a value, but writes the encoded filein to
-# fileout.
+# Doesn't return a value, but decodes the input video.
 
 def main(argv):
-
 	videoin = str(sys.argv[1])
 	res = str(sys.argv[2])
-	fileout = str(sys.argv[3])
 
-	subprocess.call("cat filein | ../C/./lvdoenc -s res -q 6 --qmin 1 --qmax 4 | x264 --input-res res --fps 1 --profile high --level 5.1 --tune stillimage --crf 22 --colormatrix bt709 --me dia --merange 0 -o fileout -", shell=True)
-
+	subprocess.call("ffmpeg -i " + videoin + " -r 1 -f rawvideo - | ./lvdodec -s " + res + " -q 6 --qmin 1 --qmax 4 | mplayer -", shell=True)
 	sys.exit()
 
 
--- src/python/encode.py
+++ src/python/encode.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 import sys
 import subprocess
 
@@ -11,13 +11,11 @@ import subprocess
 # fileout.
 
 def main(argv):
-
 	filein = str(sys.argv[1])
 	res = str(sys.argv[2])
 	fileout = str(sys.argv[3])
 
-	subprocess.call("cat " + filein + " | ../C/./lvdoenc -s " + res + " -q 6 --qmin 1 --qmax 4 | x264 --input-res " + res +" --fps 1 --profile high --level 5.1 --tune stillimage --crf 22 --colormatrix bt709 --me dia --merange 0 -o " + fileout + " -", shell=True)
-
+	subprocess.call("cat filein | ./lvdoenc -s res -q 6 --qmin 1 --qmax 4 | x264 --input-res res --fps 1 --profile high --level 5.1 --tune stillimage --crf 22 --colormatrix bt709 --me dia --merange 0 -o fileout -", shell=True)
 	sys.exit()
 
 
